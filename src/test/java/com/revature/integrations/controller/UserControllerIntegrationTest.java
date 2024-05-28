c6e4ec7cbb14ce6b34d7687cb9ecbd5c8dd688e5
caitlyn <catbouce@gmail.com>
2024-05-28 09:49:34 -0700

    update UserController test expected status code for register negative case

--- src/test/java/com/revature/integrations/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/revature/integrations/controller/UserControllerIntegrationTest.java
@@ -21,11 +21,12 @@ public class UserControllerIntegrationTest {
     public Javalin app;
 
     @BeforeAll
-    public static void setupClass(){
+    public static void setupClass() {
         cleanDatabaseTable();
     }
+
     @AfterAll
-    public static void teardownClass(){
+    public static void teardownClass() {
         cleanDatabaseTable();
     }
 
@@ -38,75 +39,78 @@ public class UserControllerIntegrationTest {
     @Test
     @DisplayName("Register::Positive")
     @Order(1)
-    public void testUserRegisterPositive(){
+    public void testUserRegisterPositive() {
 //        int randomNum = ThreadLocalRandom.current().nextInt(1000, 9999);
-        JavalinTest.test(app, (server, client)->{
-            Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","APIsTestUser");
-            requestJSON.put("password","validPassword");
+        JavalinTest.test(app, (server, client) -> {
+            Map<String, String> requestJSON = new HashMap<>();
+            requestJSON.put("username", "APIsTestUser");
+            requestJSON.put("password", "validPassword");
 
             int actualStatusCode;
             String responseBody;
-            try(Response response=client.post("/register",requestJSON)){
-                actualStatusCode=response.code();
-                responseBody= Objects.requireNonNull(response.body().string());
-                System.out.println(actualStatusCode+" :::: "+responseBody);
+            try (Response response = client.post("/register", requestJSON)) {
+                actualStatusCode = response.code();
+                responseBody = Objects.requireNonNull(response.body().string());
+                System.out.println(actualStatusCode + " :::: " + responseBody);
             }
-            Assertions.assertEquals(201,actualStatusCode);
+            Assertions.assertEquals(201, actualStatusCode);
             Assertions.assertNotNull(responseBody);
             System.out.println(responseBody);
         });
     }
+
     @Test
     @DisplayName("Login::Positive")
     @Order(2)
     public void testUserLoginPositive() {
         JavalinTest.test(app, (server, client) -> {
-            Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","apistestuser");
-            requestJSON.put("password","validPassword");
+            Map<String, String> requestJSON = new HashMap<>();
+            requestJSON.put("username", "apistestuser");
+            requestJSON.put("password", "validPassword");
             int actualStatusCode;
             String responseBody;
-            try(Response response=client.post("/login",requestJSON)){
-                actualStatusCode=response.code();
-                responseBody= Objects.requireNonNull(response.body().string());
-                System.out.println(actualStatusCode+" :::: "+responseBody);
+            try (Response response = client.post("/login", requestJSON)) {
+                actualStatusCode = response.code();
+                responseBody = Objects.requireNonNull(response.body().string());
+                System.out.println(actualStatusCode + " :::: " + responseBody);
             }
-            Assertions.assertEquals(202,actualStatusCode);
+            Assertions.assertEquals(202, actualStatusCode);
         });
     }
+
     @Test
     @DisplayName("Login::Negative - invalid password")
     public void testUserLoginNegative() {
         JavalinTest.test(app, (server, client) -> {
-            Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","apistestuser");
-            requestJSON.put("password","invalid");
+            Map<String, String> requestJSON = new HashMap<>();
+            requestJSON.put("username", "apistestuser");
+            requestJSON.put("password", "invalid");
             int actualStatusCode;
             String responseBody;
-            try(Response response=client.post("/login",requestJSON)){
-                actualStatusCode=response.code();
-                responseBody= Objects.requireNonNull(response.body().string());
-                System.out.println(actualStatusCode+" :::: "+responseBody);
+            try (Response response = client.post("/login", requestJSON)) {
+                actualStatusCode = response.code();
+                responseBody = Objects.requireNonNull(response.body().string());
+                System.out.println(actualStatusCode + " :::: " + responseBody);
             }
-            Assertions.assertEquals(400,actualStatusCode);
+            Assertions.assertEquals(400, actualStatusCode);
         });
     }
+
     @Test
     @DisplayName("Register::Negative - Blank Input")
-    public void testUserRegisterNegativeBlankInput(){
-        JavalinTest.test(app, (server, client)->{
-            Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","           ");
-            requestJSON.put("password","valid");
+    public void testUserRegisterNegativeBlankInput() {
+        JavalinTest.test(app, (server, client) -> {
+            Map<String, String> requestJSON = new HashMap<>();
+            requestJSON.put("username", "           ");
+            requestJSON.put("password", "valid");
             int actualStatusCode;
             String responseBody;
-            try(Response response=client.post("/register",requestJSON)){
-                actualStatusCode=response.code();
-                responseBody= Objects.requireNonNull(response.body().string());
-                System.out.println(actualStatusCode+" :::: "+responseBody);
+            try (Response response = client.post("/register", requestJSON)) {
+                actualStatusCode = response.code();
+                responseBody = Objects.requireNonNull(response.body().string());
+                System.out.println(actualStatusCode + " :::: " + responseBody);
             }
-            Assertions.assertEquals(500,actualStatusCode);
+            Assertions.assertEquals(400, actualStatusCode);
 //            Assertions.assertNull(responseBody);
         });
     }
