588a0fd96ec1d0bf50390c8444b8d2063d66da4b
Hero <jizlin575@gmail.com>
2024-05-28 09:53:47 -0400

    added unit test user controller cases

--- src/test/java/com/revature/integrations/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/revature/integrations/controller/UserControllerIntegrationTest.java
@@ -5,9 +5,7 @@ import com.revature.utilities.RequestMapper;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import okhttp3.Response;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -16,56 +14,36 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static com.revature.selenium.utils.DriverUtils.cleanDatabaseTable;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserControllerIntegrationTest {
     public Javalin app;
-    public Connection connection;
+
+    @BeforeAll
+    public static void setupClass(){
+        cleanDatabaseTable();
+    }
+    @AfterAll
+    public static void teardownClass(){
+        cleanDatabaseTable();
+    }
 
     @BeforeEach
     public void setup() throws SQLException {
-//        connection = ConnectionUtil.createConnection();
         app = Javalin.create();
         RequestMapper.setUpEndPoints(app);
     }
 
     @Test
-    public void testUserLoginPositive() {
-        JavalinTest.test(app, (server, client) -> {
-            Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","test");
-            requestJSON.put("password","test");
-            int actualStatusCode;
-            String responseBody;
-            try(Response response=client.post("/login",requestJSON)){
-                actualStatusCode=response.code();
-                responseBody= Objects.requireNonNull(response.body().string());
-                System.out.println(actualStatusCode+" :::: "+responseBody);
-            }
-            Assertions.assertEquals(202,actualStatusCode);
-        });
-    }
-    @Test
-    public void testUserLoginNegative() {
-        JavalinTest.test(app, (server, client) -> {
-            Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","test");
-            requestJSON.put("password","invalid");
-            int actualStatusCode;
-            String responseBody;
-            try(Response response=client.post("/login",requestJSON)){
-                actualStatusCode=response.code();
-                responseBody= Objects.requireNonNull(response.body().string());
-                System.out.println(actualStatusCode+" :::: "+responseBody);
-            }
-            Assertions.assertEquals(400,actualStatusCode);
-        });
-    }
-    @Test
+    @DisplayName("Register::Positive")
+    @Order(1)
     public void testUserRegisterPositive(){
-        int randomNum = ThreadLocalRandom.current().nextInt(1000, 9999);
+//        int randomNum = ThreadLocalRandom.current().nextInt(1000, 9999);
         JavalinTest.test(app, (server, client)->{
             Map<String,String> requestJSON=new HashMap<>();
-            requestJSON.put("username","APIsTest"+randomNum);
-            requestJSON.put("password","validpwd");
+            requestJSON.put("username","APIsTestUser");
+            requestJSON.put("password","validPassword");
 
             int actualStatusCode;
             String responseBody;
@@ -80,6 +58,42 @@ public class UserControllerIntegrationTest {
         });
     }
     @Test
+    @DisplayName("Login::Positive")
+    @Order(2)
+    public void testUserLoginPositive() {
+        JavalinTest.test(app, (server, client) -> {
+            Map<String,String> requestJSON=new HashMap<>();
+            requestJSON.put("username","apistestuser");
+            requestJSON.put("password","validPassword");
+            int actualStatusCode;
+            String responseBody;
+            try(Response response=client.post("/login",requestJSON)){
+                actualStatusCode=response.code();
+                responseBody= Objects.requireNonNull(response.body().string());
+                System.out.println(actualStatusCode+" :::: "+responseBody);
+            }
+            Assertions.assertEquals(202,actualStatusCode);
+        });
+    }
+    @Test
+    @DisplayName("Login::Negative - invalid password")
+    public void testUserLoginNegative() {
+        JavalinTest.test(app, (server, client) -> {
+            Map<String,String> requestJSON=new HashMap<>();
+            requestJSON.put("username","apistestuser");
+            requestJSON.put("password","invalid");
+            int actualStatusCode;
+            String responseBody;
+            try(Response response=client.post("/login",requestJSON)){
+                actualStatusCode=response.code();
+                responseBody= Objects.requireNonNull(response.body().string());
+                System.out.println(actualStatusCode+" :::: "+responseBody);
+            }
+            Assertions.assertEquals(400,actualStatusCode);
+        });
+    }
+    @Test
+    @DisplayName("Register::Negative - Blank Input")
     public void testUserRegisterNegativeBlankInput(){
         JavalinTest.test(app, (server, client)->{
             Map<String,String> requestJSON=new HashMap<>();
@@ -92,7 +106,7 @@ public class UserControllerIntegrationTest {
                 responseBody= Objects.requireNonNull(response.body().string());
                 System.out.println(actualStatusCode+" :::: "+responseBody);
             }
-            Assertions.assertEquals(201,actualStatusCode);
+            Assertions.assertEquals(500,actualStatusCode);
 //            Assertions.assertNull(responseBody);
         });
     }
---
+++ src/test/java/com/revature/units/controller/UserControllerTest.java
@@ -0,0 +1,116 @@
+package com.revature.units.controller;
+
+import com.revature.controller.UserController;
+import com.revature.models.User;
+import com.revature.models.UsernamePasswordAuthentication;
+import com.revature.service.UserService;
+import io.javalin.http.Context;
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+
+public class UserControllerTest {
+    @Mock
+    UserService userService;
+    @InjectMocks
+    UserController userController;
+
+    private final Context ctx = mock(Context.class);
+    @BeforeEach
+    public void setup(){
+        userService = Mockito.mock(UserService.class);
+        userController = new UserController(userService);
+    }
+
+    @Test
+    @DisplayName("Register::Positive")
+    public void testUserRegisterPositive() {
+        User userInput = new User();
+        userInput.setUsername("testUserRegisterPositive");
+        userInput.setPassword("valid");
+        User userReturn = new User();
+        userReturn.setId(100);
+        userReturn.setUsername("testUserRegisterPositive");
+        userReturn.setPassword("valid");
+        Mockito.when(ctx.bodyAsClass(User.class)).thenReturn(userInput);
+        Mockito.when(userService.register(any())).thenReturn(userReturn);
+        Mockito.when(ctx.json(userReturn)).thenReturn(ctx);
+//        Mockito.when(ctx.status()).thenReturn(HttpStatus.forStatus(201));
+//        Mockito.doNothing().when(ctx.json(new Object()));
+        userController.register(ctx);
+        Mockito.verify(ctx).status(201);
+        Mockito.verify(ctx).json(userReturn);
+    }
+
+    @Test
+    @DisplayName("Login::Positive")
+    public void testUserLoginPositive() {
+        UsernamePasswordAuthentication userInput = new UsernamePasswordAuthentication();
+        userInput.setUsername("testUserRegisterPositive");
+        userInput.setPassword("valid");
+        User userReturn = new User();
+        userReturn.setId(100);
+        userReturn.setUsername("testUserRegisterPositive");
+        userReturn.setPassword("valid");
+        Mockito.when(ctx.bodyAsClass(UsernamePasswordAuthentication.class)).thenReturn(userInput);
+        Mockito.when(userService.authenticate(any())).thenReturn(userReturn);
+        userController.authenticate(ctx);
+        Mockito.verify(ctx).status(202);
+        Mockito.verify(ctx).json(userReturn);
+
+
+
+    }
+
+    @Test
+    @DisplayName("Login::Negative - invalid password")
+    public void testUserLoginNegative() {
+        UsernamePasswordAuthentication userInput = new UsernamePasswordAuthentication();
+        userInput.setUsername("testUserRegisterPositive");
+        userInput.setPassword("valid");
+        User userReturn = new User();
+        userReturn.setId(100);
+        userReturn.setUsername("testUserRegisterPositive");
+        userReturn.setPassword("valid");
+        Mockito.when(ctx.bodyAsClass(UsernamePasswordAuthentication.class)).thenReturn(userInput);
+        Mockito.when(userService.authenticate(any())).thenReturn(null);
+        userController.authenticate(ctx);
+        Mockito.verify(ctx).status(400);
+//        Mockito.verify(ctx).json();
+
+    }
+
+    @Test
+    @DisplayName("Register::Negative - Blank Input")
+    public void testUserRegisterNegativeBlankInput() {
+        User userInput = new User();
+        userInput.setUsername("         ");
+        userInput.setPassword("valid");
+        User userReturn = new User();
+        Mockito.when(ctx.bodyAsClass(User.class)).thenReturn(userInput);
+        Mockito.when(userService.register(any())).thenReturn(userReturn);
+        Mockito.when(ctx.json(userReturn)).thenReturn(ctx);
+        userController.register(ctx);
+        Mockito.verify(ctx).status(201);
+        Mockito.verify(ctx).json(userReturn);
+
+    }
+    @Test
+    @DisplayName("Register::Negative - Throw NullPointerException")
+    public void testUserRegisterNegativeThrowException() {
+        User userInput = new User();
+        userInput.setUsername("         ");
+        userInput.setPassword("valid");
+        User userReturn = null;
+        Mockito.when(ctx.bodyAsClass(User.class)).thenReturn(userInput);
+        Mockito.when(userService.register(any())).thenReturn(userReturn);
+//        Mockito.when(ctx.json(null)).thenReturn(ctx);
+        Assertions.assertThrows(NullPointerException.class, ()->{
+            userController.register(ctx);
+        });
+    }
+}
